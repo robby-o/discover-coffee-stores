3da61372bd20c7632f747e46461baac20cdedce7
robby-o <robby.olson11@gmail.com>
2023-02-23 18:27:18 -0800

    testing for existence of name

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -100,7 +100,9 @@ const CoffeeStore = (initialProps = null) => {
     }
   }, [id, initialProps.coffeeStore, coffeeStores])
 
-  const { name = 'Coffee Shop', address = '', imgUrl = '' } = coffeeStore
+  const name = coffeeStore?.name || ''
+  const address = coffeeStore?.address || ''
+  const imgUrl = coffeeStore?.imgUrl || ''
   const [votingCount, setVotingCount] = useState(0)
 
   const { data, error } = useSWR(
@@ -146,70 +148,66 @@ const CoffeeStore = (initialProps = null) => {
     return <div>Something went wrong retrieving coffee store page</div>
   }
 
-  if (coffeeStore && coffeeStore.name) {
-    return (
-      <div className={styles.layout}>
-        <Head>
-          <title>{name}</title>
-          <meta name='description' content={`${name} coffee store`} />
-        </Head>
-        <div className={styles.container}>
-          <div className={styles.col1}>
-            <div className={styles.backToHomeLink}>
-              <Link href='/'>← Back to home</Link>
-            </div>
-            <div className={styles.nameWrapper}>
-              <h1 className={styles.name}>{name}</h1>
-            </div>
-            <Image
-              src={
-                imgUrl ||
-                'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
-              }
-              width={600}
-              height={360}
-              className={styles.storeImg}
-              alt={name}
-              priority={true}
-            />
+  return (
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+        <meta name='description' content={`${name} coffee store`} />
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href='/'>← Back to home</Link>
           </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+            }
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+            priority={true}
+          />
+        </div>
 
-          <div className={cls('glass', styles.col2)}>
-            {address && (
-              <div className={styles.iconWrapper}>
-                <Image
-                  src='/static/icons/places.svg'
-                  width='24'
-                  height='24'
-                  alt='places icon'
-                />
-                <p className={styles.text}>{address}</p>
-              </div>
-            )}
-
+        <div className={cls('glass', styles.col2)}>
+          {address && (
             <div className={styles.iconWrapper}>
               <Image
-                src='/static/icons/star.svg'
+                src='/static/icons/places.svg'
                 width='24'
                 height='24'
-                alt='star icon'
+                alt='places icon'
               />
-              <p className={styles.text}>{votingCount}</p>
+              <p className={styles.text}>{address}</p>
             </div>
+          )}
 
-            <button
-              className={styles.upvoteButton}
-              onClick={handleUpvoteButton}
-            >
-              Up vote!
-            </button>
+          <div className={styles.iconWrapper}>
+            <Image
+              src='/static/icons/star.svg'
+              width='24'
+              height='24'
+              alt='star icon'
+            />
+            <p className={styles.text}>{votingCount}</p>
           </div>
+
+          <button
+            className={styles.upvoteButton}
+            onClick={handleUpvoteButton}
+          >
+            Up vote!
+          </button>
         </div>
       </div>
-    )
-  }
-
-  return <p>Loading...</p>
+    </div>
+  )
 }
 
 export default CoffeeStore
